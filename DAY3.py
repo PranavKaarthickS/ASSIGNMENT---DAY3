aa3e0baf9fc90e909438ea9c2ac748140868b4a7
PranavKaarthickS <98024988+PranavKaarthickS@users.noreply.github.com>
2022-01-20 15:05:21 +0530

    Add files via upload

---
+++ DAY3.py
@@ -0,0 +1,13 @@
+name = input("Enter Your Name")
+age = input("Enter Your Age")
+marks = input("Enter Your Marks")
+Location = input("Which Country Are You From")
+Salary = input("What Is Your Salary")
+print("Your Details are")
+
+
+print (f"Your Name Is:",name)
+print(f"Your Age Is:",age)
+print(f"Your Mark Is:",marks)
+print(f"You Are From:",Location)
+print(f"Your Salary Is:",Salary)
